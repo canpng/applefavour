4fc840a99b607dfba9fcd15b2320c6ae68232381
canpng <49827841+canpng@users.noreply.github.com>
2023-04-30 19:51:50 +0300

    Delete templates/website directory

--- templates/website/Neonight-V1/assist/js/Neonight.js
+++
@@ -1,4 +0,0 @@
-var boxes=document.querySelectorAll("#bg-box");for(i=0;i<boxes.length;i++){animateMe(boxes[i])}
-function animateMe(box){TweenMax.to(box,randomNumber(1,4),{opacity:randomNumber(0,0.8),delay:randomNumber(1,3),repeatDelay:randomNumber(1,2),repeat:1,yoyoEase:true,onComplete:animateMe,onCompleteParams:[box]})}
-animateMe(document.querySelector("h1"))
-function randomNumber(min,max){return Math.random()*(max-min)+min;}
